c133d4d9db031ed7e1e63ce07eeff479ab25d0ca
Max <max.heimbrock@gmx.de>
2019-08-10 19:31:14 +0200

    ex 5 finished

--- OpenCV-Project/Client.cpp
+++ OpenCV-Project/Client.cpp
@@ -165,7 +165,7 @@ int main(int argc, const char * argv[]) {
 						// Draw polygon around contour with 4 points
 						//cv::polylines(frame, approx, true, Scalar(0, 0, 255), 4);
 
-						vector<Mat> fittedLines(4);
+						vector<vector<double>> fittedLines(4);
 				
 						// Divide polygon lines into 7 parts with 6 points and endpoints
 						// For every line of square
@@ -277,28 +277,188 @@ int main(int argc, const char * argv[]) {
 								// Fill the vector for each square intermediate points
 								intermediatePoints[a-1] = edgeCenter;
 
-								// Highlight the subpixel with blue color
+								// Draw / Highlight the subpixel with blue color
 								circle(frame, edgeCenter, 2, CV_RGB(0, 0, 255), -1);
 							}
 
 							// Fit the line and add it to the other fitted lines of the square
-							Mat fittedLine;
+							vector<double> fittedLine;
 							fitLine(intermediatePoints, fittedLine, CV_DIST_L2, 0, 0.01, 0.01);
 							fittedLines[j] = fittedLine;
 
 							// draw endpoint
-							circle(frame, Point(approx[j].x, approx[j].y), 1, Scalar(0, 255, 0), 2, 8);
+							//circle(frame, Point(approx[j].x, approx[j].y), 1, Scalar(0, 255, 0), 2, 8);
+						}	
+
+						// Here I have all lines fitted in fittedLines
+						//------------------------------
+
+						Point2f corners[4];
+
+						// Calculate the intersection points of both lines
+						// For every line of square
+						for (int j = 0; j < 4; ++j) 
+						{
+							// Index of second line 
+							int jj = (j + 1) % 4;
+
+							// We look for:
+							// g0 = (x0,y0) + a*(u0,v0) == g1 = (x1,y1) + b*(u1,v1)
+
+							double x0, x1, y0, y1, u0, u1, v0, v1;
+
+							// From first line
+							u0 = fittedLines[j][0];
+							v0 = fittedLines[j][1];
+							x0 = fittedLines[j][2];
+							y0 = fittedLines[j][3];
+
+							// From second line
+							u1 = fittedLines[jj][0];
+							v1 = fittedLines[jj][1];
+							x1 = fittedLines[jj][2];
+							y1 = fittedLines[jj][3];
+
+							// (x|y) = p + s * vec
+							// s = Ds / D (see Cramer's rule)
+							// (x|y) = p + (Ds / D) * vec
+							// (x|y) = (p * D / D) + (Ds * vec / D)
+							// (x|y) = (p * D + Ds * vec) / D
+							// (x|y) = a / c;
+
+							// Cramer's rule
+							// 2 unknown a,b -> Equation system
+							double a = x1 * u0 * v1 - y1 * u0 * u1 - x0 * u1 * v0 + y0 * u0 * u1;
+							double b = -x0 * v0 * v1 + y0 * u0 * v1 + x1 * v0 * v1 - y1 * v0 * u1;
+
+							// Calculate the cross product to check if both direction vectors are parallel -> = 0
+							// c -> Determinant = 0 -> linear dependent -> the direction vectors are parallel -> No division with 0
+							double c = v1 * u0 - v0 * u1;
+							if (fabs(c) < 0.001) {
+								std::cout << "lines parallel" << std::endl;
+								continue;
+							}
+
+							// We have checked for parallelism of the direction vectors
+							// -> Cramer's rule, now divide through the main determinant
+							a /= c;
+							b /= c;
+
+							// Exact corner
+							corners[j].x = a;
+							corners[j].y = b;
+
+							// Added in sheet 4 Ex9 (b)- End *******************************************************************
+
+							Point p;
+							p.x = (int)corners[j].x;
+							p.y = (int)corners[j].y;
+
+							// Added in sheet 4 Ex9 (c)- Start *****************************************************************
+
+							circle(frame, p, 5, CV_RGB(255, 255, 0), -1);
 						}
 
-						// Here I have all lines fitted
+						// We now have the corners in corners[]
+						//-------------------------------------
 
+						// Calculate homeography to target corners
+						Point2f targetCorners[4];
+						targetCorners[0].x = -0.5; targetCorners[0].y = -0.5;
+						targetCorners[1].x = 5.5; targetCorners[1].y = -0.5;
+						targetCorners[2].x = 5.5; targetCorners[2].y = 5.5;
+						targetCorners[3].x = -0.5; targetCorners[3].y = 5.5;
 
+						Mat H = getPerspectiveTransform(corners, targetCorners);
+
+						// Create image for the marker
+						Mat imageMarker(Size(6, 6), CV_8UC1);
+
+						warpPerspective(frameGray, imageMarker, H, Size(6, 6));
+						threshold(imageMarker, imageMarker, 90, 255, ADAPTIVE_THRESH_MEAN_C);
+						
+						/*
+						Mat enlarged;
+
+						cv::resize(imageMarker, enlarged, cv::Size(imageMarker.cols * 10, imageMarker.rows * 10), cv::INTER_NEAREST);
+
+						cv::imshow("Mai Frame", enlarged);
+						*/
+
+						int code = 0;
+						for (int j = 0; j < 6; ++j) {
+							// Check if border is black
+							int pixel1 = imageMarker.at<uchar>(0, j); //top
+							int pixel2 = imageMarker.at<uchar>(5, j); //bottom
+							int pixel3 = imageMarker.at<uchar>(j, 0); //left
+							int pixel4 = imageMarker.at<uchar>(j, 5); //right
+
+							// 0 -> black
+							if ((pixel1 > 0) || (pixel2 > 0) || (pixel3 > 0) || (pixel4 > 0)) {
+								code = -1;
+								break;
+							}
+						}
+
+						if (code < 0) {
+							continue;
+						}
+
+						// Copy the BW values into cP -> codePixel on the marker 4x4 (inner part of the marker, no black border)
+						int cP[4][4];
+						for (int j = 0; j < 4; ++j) {
+							for (int k = 0; k < 4; ++k) {
+								// +1 -> no borders!
+								cP[j][k] = imageMarker.at<uchar>(j + 1, k + 1);
+								// If black then 1 else 0
+								cP[j][k] = (cP[j][k] == 0) ? 1 : 0;
+							}
+						}
+
+						// Save the ID of the marker, for each side
+						int codes[4];
+						codes[0] = codes[1] = codes[2] = codes[3] = 0;
+
+						// Calculate the code from all sides at once
+						for (int j = 0; j < 16; j++) {
+							// /4 to go through the rows
+							int row = j >> 2;
+							int col = j % 4;
+
+							// Multiplied by 2 to check for black values -> 0*2 = 0
+							codes[0] <<= 1;
+							codes[0] |= cP[row][col]; // 0°
+
+							// 4x4 structure -> Each column represents one side 
+							codes[1] <<= 1;
+							codes[1] |= cP[3 - col][row]; // 90°
+
+							codes[2] <<= 1;
+							codes[2] |= cP[3 - row][3 - col]; // 180°
+
+							codes[3] <<= 1;
+							codes[3] |= cP[col][3 - row]; // 270°
+						}
+
+						// Account for symmetry -> One side complete white or black
+						if ((codes[0] == 0) || (codes[0] == 0xffff)) {
+							continue;
+						}
+
+						// Search for the smallest marker ID
+						code = codes[0];
+						for (int j = 1; j < 4; ++j) {
+							if (codes[j] < code) {
+								code = codes[j];
+							}
+						}
 					}
 				}
 			}							
 		}
 
 		cv::imshow("Main Frame", frame);
+
 		//imshow("Thresholded Frame", frameThresholded);
 
 		char key = (char)cv::waitKey(30);   // explicit cast
